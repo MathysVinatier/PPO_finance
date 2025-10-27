6a373fefa47b078c850b38615d7cddc56289132c
MathysVinatier <vinmat2002@gmail.com>
2025-10-27 14:43:47 +0900

    indepedent importation

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,29 @@
-from .Environment import TradingEnv, DataLoader
 
-from .RLMethod import QLearning, DeepQLearning, PPOAgent
-from .PPO import ACAgent
+__all__ = [
+    "TradingEnv",
+    "DataLoader",
+    "QLearning",
+    "DeepQLearning",
+    "PPOAgent",
+    "ACAgent",
+    "ModelReport",
+]
+
+def __getattr__(name):
+    if name == "TradingEnv" or name == "DataLoader":
+        from .Environment import TradingEnv, DataLoader
+        return locals()[name]
+
+    if name in {"QLearning", "DeepQLearning", "PPOAgent"}:
+        from .RLMethod import QLearning, DeepQLearning, PPOAgent
+        return locals()[name]
+
+    if name == "ACAgent":
+        from .PPO import ACAgent
+        return ACAgent
+
+    if name == "ModelReport":
+        from .PPO_analysis import ModelReport
+        return ModelReport
+
+    raise AttributeError(f"module {__name__} has no attribute {name}")
